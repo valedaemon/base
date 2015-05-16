a8bf49d163bec3a275162b54515bcea9176c21da
JJ Johnson <valedaemon@gmail.com>
2015-05-15 21:14:33 -0400

    Initial

---
+++ server/collections.js
@@ -0,0 +1,46 @@
+Posts = new Mongo.Collection("posts");
+Schema = {};
+
+
+Schema.Posts = new SimpleSchema({
+  title: {
+    type: String,
+    max: 60
+  },
+  content: {
+    type: String,
+    autoform: {
+      rows: 5
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: 'Date',
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date();
+      }
+    }
+  },
+  owner: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    autoValue: function() {
+      if (this.isInsert) {
+        return Meteor.userId();
+      }
+    },
+    autoform: {
+      options: function() {
+        return _.map(Meteor.users.find().fetch(), function(user) {
+          return {
+            label: user.emails[0].address,
+            value: user._id
+          };
+        });
+      }
+    }
+  }
+});
+
+Posts.attachSchema(Schema.Posts);
